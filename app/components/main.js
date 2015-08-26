8acca6f8b8be995294df957f982fffddd66b8dc3
pompopo <pompopo@gmail.com>
2015-08-27 02:15:48 +0900

    add logger

--- app/components/main.js
+++ app/components/main.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/index.js'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 
 let storedState = {
   github: {
     auth_token: localStorage.auth_token
   }
 };
-console.log('state' + require('util').inspect(storedState));
-
-let store = applyMiddleware(thunk)(createStore)(rootReducer, storedState);
+let logger = createLogger();
+let store = applyMiddleware(thunk, logger)(createStore)(rootReducer, storedState);
 React.render(
   <Provider store={store}>
     {() => <Root />}
