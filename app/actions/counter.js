d518c77f82be7c2e7d07f0ac3b0d002f4fa2f00d
pompopo <hiroki_takemoto@fablic.co.jp>
2015-08-26 19:23:05 +0900

    GitHubにログインできるように変更

--- app/actions/counter.js
+++ app/actions/counter.js
@@ -1,3 +1,7 @@
+import github from 'octonode'
+var remote = require('remote');
+var BrowserWindow = remote.require('browser-window');
+
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const ASYNC_INCREMENT = 'ASYNC_INCREMENT';
@@ -16,8 +20,33 @@ export function decrement() {
 
 export function async_increment() {
   return dispatch => {
+    authenticate();
     setTimeout(() => {
       dispatch(increment());
     }, 1000);
   }
+}
+
+function authenticate() {
+  var auth_url = github.auth.config({
+    id: '358f22de71598257b9df',
+    secret: 'f759df34aa2f1dedbe6f07103bb46dfd1d5002b6'
+  }).login(['user', 'repo', 'gist']);
+
+  var state = auth_url.match(/&state=([0-9a-z]{32})/i);
+
+  var window = new BrowserWindow({ width: 800, height: 600});
+  window.webContents.on('will-navigate', (event, url) => {
+
+    let matched;
+    if (matched = url.match(/\?code=([^&]*)&state=([^&]*)/)) {
+      let code = matched[1];
+      github.auth.login(code, function (err, token) {
+        window.close();
+      });
+
+    };
+  });
+
+  window.loadUrl(auth_url)
 }
