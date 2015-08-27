1aca134f6f7155a7d5eec3f05493f30b5fba7b0d
pompopo <pompopo@gmail.com>
2015-08-28 01:42:07 +0900

    アイコンを使ったり

--- app/components/events/event.js
+++ app/components/events/event.js
@@ -7,10 +7,33 @@ export default class Event extends React.Component {
     let e = this.props.event;
     return (
       <div>
-        <img className="avatar" src={e.actor.avatar_url}/>
+        {this._icon(e)}
         <span className="repository">{e.repo.name}</span>{this._makeMessage(e)}
       </div>
     );
+  }
+
+  _icon(event) {
+    switch(event.type) {
+      case 'PushEvent':
+        return (<span className="octicon octicon-repo-push"></span>)
+      case 'IssueCommentEvent':
+      case 'PullRequestReviewCommentEvent':
+        return (<span className="octicon octicon-comment"></span>)
+      case 'PullRequestEvent':
+        return (<span className="octicon octicon-git-pull-request"></span>)
+      case 'IssuesEvent':
+        return (<span className="octicon octicon-issue-opened"></span>)
+      case 'DeleteEvent':
+        return (<span className="octicon octicon-trashcan"></span>);
+      case 'CreateEvent':
+        // FIXME
+        return (<span class="octicon octicon-plus"></span>);
+      case 'WatchEvent':
+        return (<span className="octicon octicon-star"></span>)
+      default:
+        return (<span></span>)
+    }
   }
 
   _openCommit(commit, event) {
@@ -35,6 +58,14 @@ export default class Event extends React.Component {
         return 'PRコメント - ' + event.payload.comment.body;
       case 'PullRequestEvent':
         return 'PullRequest ' + event.payload.pull_request.title;
+      case 'IssuesEvent':
+        return event.payload.action + ' ' + event.payload.issue.title;
+      case 'DeleteEvent':
+        return 'delete ' + event.payload.ref_type + ' ' + event.payload.ref;
+      case 'CreateEvent':
+        return 'create ' + event.payload.ref_type + ' ' + event.payload.ref;
+      case 'WatchEvent':
+        return 'starred ' + event.repo.name;
       default:
         return event.type;
     }
